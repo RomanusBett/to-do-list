1e705679d337c5a509a47a487d26d73d9655124e
Romanus Bett <romanus@Kevins-MacBook-Pro.local>
2021-09-12 00:13:38 +0300

    add styles to app

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const app = express();
-var items = ["Buy food", "Eat food", "Sleep"];
+let items = ["Buy food", "Eat food", "Sleep"];
+let workItems = [];
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", function(req, res) {
-var today = new Date();
-var options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long",
-};
-var day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 res.render("list", {
-  kindOfDay: day, newListItems: items
+  listTitle: day, newListItems: items
 });
 });
 app.post("/", function(req, res){
-  var item = req.body.newItem;
-  items.push(item);
-res.redirect("/");
+  let item = req.body.newItem;
+  if (req.body.list==="Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work Day", newListItems: workItems});
+});
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+app.get("/about", function(req, res){
+  res.render("about");
 });
 app.listen(5000, function() {
   console.log("the server is running on port 5000");
